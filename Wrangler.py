dbcc1d678e70804f7e6492f45cdfb30c68f4587b
Caruy93 <58788447+Caruy93@users.noreply.github.com>
2020-02-03 15:00:47 +0200

    Add files via upload

---
+++ Wrangler.py
@@ -0,0 +1,29 @@
+import re
+import os
+# Set Functions and parameters
+## Extract elements into a list, and parse observations to elements
+def extractLines(lst):
+    return [el.split() for el in lst]
+
+## Perform multiple 1:1 string replacements using dict
+def multipleReplace(dict, text):
+  # Create a regular expression  from the dictionary keys
+  regex = re.compile("(%s)" % "|".join(map(re.escape, dict.keys())))
+
+  # For each match, look-up corresponding value in dictionary
+  return regex.sub(lambda mo: dict[mo.string[mo.start():mo.end()]], text)
+
+def makeDirFile(file):
+    if not file.endswith('.csv'):
+        outname = file + '.csv'
+    else:
+        outname = file
+
+    parent = os.path.dirname(os.path.dirname(os.path.abspath('__file__')))
+    outdir = parent + '/Data'
+
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+        print('Created new directory:',outdir)
+
+    return os.path.join(outdir, outname)
